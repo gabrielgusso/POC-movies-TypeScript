eaff0b37a69fda7f7ca24c3c5285230f79c5aad0
gabrielgusso <gabriel.gusso@hotmail.com>
2023-01-21 00:52:13 -0300

    Feat: movies DELETE and UPDATE routes

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -19,6 +19,18 @@ export async function movieGetController(req: Request, res: Response) {
   }
 }
 
+export async function totalMovieGetController(req: Request, res: Response) {
+  try {
+    const { rows } = await connection.query(`
+        SELECT COUNT(id) as "totalMovies" FROM movies
+        `)
+
+    return res.status(200).send(rows[0])
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+
 export async function moviePostController(req: Request, res: Response) {
   const movie = req.body as Movie
 
@@ -31,6 +43,55 @@ export async function moviePostController(req: Request, res: Response) {
     )
 
     res.sendStatus(201)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+
+export async function movieUpdateController(req: Request, res: Response) {
+  const id = req.params.id
+  const isNum = /^\d+$/.test(id)
+  if (!isNum) {
+    res.sendStatus(400)
+    return
+  }
+  try {
+    const verifyIfExist = await connection.query(
+      `SELECT FROM movies WHERE id=$1`,
+      [id]
+    )
+    if (!verifyIfExist.rows[0]) {
+      return res.status(404).send("Movie not found")
+    }
+
+    await connection.query(
+      `UPDATE movies SET "watchedStatus" = true WHERE id = $1`,
+      [id]
+    )
+    res.sendStatus(200)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+
+export async function movieDeleteController(req: Request, res: Response) {
+  const id = req.params.id
+  const isNum = /^\d+$/.test(id)
+  if (!isNum) {
+    res.sendStatus(400)
+    return
+  }
+  try {
+    const verifyIfExist = await connection.query(
+      `SELECT FROM movies WHERE id=$1`,
+      [id]
+    )
+    if (!verifyIfExist.rows[0]) {
+      return res.status(404).send("Movie not found")
+    }
+
+    await connection.query(`DELETE FROM movies WHERE id = $1`, [id])
+    res.sendStatus(200)
   } catch (error) {
     res.status(500).send(error)
   }
--- src/routers/movies.routers.ts
+++ src/routers/movies.routers.ts
@@ -2,6 +2,9 @@ import { Router } from "express"
 import {
   movieGetController,
   moviePostController,
+  movieUpdateController,
+  movieDeleteController,
+  totalMovieGetController
 } from "../controllers/movie.controller.js"
 import { movieMiddleware } from "../middlewares/movie.middleware.js"
 
@@ -9,4 +12,11 @@ export const movieRouter = Router()
 
 movieRouter.get("/movies", movieGetController)
 
+movieRouter.get("/total-movies", totalMovieGetController)
+
 movieRouter.post("/movie", movieMiddleware, moviePostController)
+
+movieRouter.patch("/movie/:id", movieUpdateController)
+
+movieRouter.delete("/movie/:id", movieDeleteController)
+
